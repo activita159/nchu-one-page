82b1e44353f19bb52e60ba5f3003bc69d4631b17
AllenWang <activita159@gmail.com>
2026-06-18 14:16:38 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/nchu-one-page/',
   plugins: [react()],
   build: {
     rollupOptions: {
